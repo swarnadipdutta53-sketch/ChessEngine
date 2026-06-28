78e70bef41c23781eaaf17d6568b96f5c77e021c
swarnadipdutta53-sketch <swarnadipdutta53@gmail.com>
2026-06-28 22:33:25 +0530

    *.h

--- pieces.h
+++ pieces.h
@@ -1,12 +1,12 @@
 #include "Coords.h"
 #ifndef t_PIECES
 #define t_PIECES
-class Pieces
+typedef struct Pieces
 {
     private:
     public:
     Coords coords;
     char type;
-    char team;
-};
+    char team; 
+}Pieces;
 #endif
