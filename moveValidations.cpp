f5b796dee2089230de5fec42a64bb446c55340c1
swarnadipdutta53-sketch <swarnadipdutta53@gmail.com>
2026-07-05 13:31:08 +0530

    *.*

--- moveValidations.cpp
+++ moveValidations.cpp
@@ -1,10 +1,10 @@
-//TODO: REMOVING THE CAPTURED PAWN FROM THE BOARD(implementation of enum in return values) & THREAT CHECKS
+//TODO: THREAT CHECKS
 
 #include "moveValidations.h"
 #include "Board.h"
 #include "stdiodef.h"
 
-bool moveValidationRook(int row, int column, Coords c2, Board& obj)
+ReturnType moveValidationRook(int row, int column, Coords c2, Board& obj)
 {
     // if valid
     if(row == c2.x || column == c2.y)
@@ -24,23 +24,23 @@ bool moveValidationRook(int row, int column, Coords c2, Board& obj)
         {
             if(obj.getpiece(xi, yi) != nullptr)
             {
-                return false; // obstruction faced
+                return invalid; // obstruction faced
             }
             xi += dx;
             yi += dy;
         }
         if(obj.getpiece(c2.x, c2.y) == nullptr)
-            return true;
+            return gen_valid;
         else if(obj.getpiece(c2.x,c2.y)->team != obj.getpiece(row,column) -> team)
-            return true; // capture case
+            return gen_valid; // capture case
         else
-            return false; // destination is obstruction itself
+            return invalid; // destination is obstruction itself
     }
     else
-        return false; // destination is not parallel
+        return invalid; // destination is not parallel
 }
 
-bool moveValidationBishop(int row, int column, Coords c2, Board& obj)
+ReturnType moveValidationBishop(int row, int column, Coords c2, Board& obj)
 {
     // if valid
     /*
@@ -56,38 +56,38 @@ bool moveValidationBishop(int row, int column, Coords c2, Board& obj)
         while(c2.x != xi || c2.y != yi)
         {
             if(obj.getpiece(xi,yi) != nullptr)
-                return false; // obstruction faced
+                return invalid; // obstruction faced
             xi += dx;
             yi += dy;
         }
         if(obj.getpiece(c2.x,c2.y) == nullptr)
-            return true;
+            return gen_valid;
         else if(obj.getpiece(c2.x,c2.y)-> team != obj.getpiece(row, column) -> team)
-            return true; // capture case
+            return gen_valid; // capture case
         else
-            return false; // destination is obstruction itself
+            return invalid; // destination is obstruction itself
     }
     else
-        return false; // destination is not diagonal
+        return invalid; // destination is not diagonal
 }
 
-bool moveValidationKnight(int row, int column, Coords c2, Board &obj)
+ReturnType moveValidationKnight(int row, int column, Coords c2, Board &obj)
 {
     int dx, dy;
     dx = c2.x - row;
     dy = c2.y - column;
-    // forwards and backwards
+    // forwards and backwards(first layer check: general validation)
     if((abs(dx) == 2 && abs(dy) == 1) || (abs(dx) == 1 && abs(dy) == 2))
     {
         if(obj.getpiece(c2.x, c2.y) == nullptr)
-            return true;
+            return gen_valid;
         else if(obj.getpiece(c2.x, c2.y)-> team != obj.getpiece(row, column)-> team)
-            return true;
+            return gen_valid;
     }
-    return false; // invalidated
+    return invalid; // invalidated
 }
 
-bool moveValidationPawn(int row, int column, Coords c2, Board &obj)
+ReturnType moveValidationPawn(int row, int column, Coords c2, Board &obj)
 {
     Pieces* lastPiece = obj.getlastmovedpiece();
     int dx, dy, step;
@@ -95,68 +95,66 @@ bool moveValidationPawn(int row, int column, Coords c2, Board &obj)
     dy = c2.y - column;
     // direction validation
     if(dx > 0 && obj.getpiece(row, column)-> team == 'p')
-        return false;
+        return invalid;
     else if(dx < 0 && obj.getpiece(row, column)-> team == 'P')
-        return false;
+        return invalid;
     // double forward
     if(!(obj.getpiece(row, column)->hasMoved) && abs(dx) == 2 && dy == 0 && obj.getpiece(c2.x,c2.y) == nullptr)
     {
         step = (c2.x > row) ? 1 : -1;
         if(!(obj.getpiece(row + step, column))) // no obstruction in the intermediate square
-            return true;
+            return gen_valid;
     }
     // forwards
     else if((abs(dx) == 1 && dy == 0))
     {
         if(obj.getpiece(c2.x, c2.y) == nullptr)
-            return true;
+            return gen_valid;
     }
     // diagonal capture
     else if((abs(dx) == 1 && abs(dy) == 1))
     {
         // en passant diagonal capture
         Pieces* adjacent = obj.getpiece(row, column + dy);
-        Pieces* lastPiece = obj.getlastmovedpiece();
         if (adjacent != nullptr && lastPiece != nullptr)
         {
             if (adjacent == lastPiece &&
                 adjacent->team != obj.getpiece(row, column)->team &&
                 adjacent->type != obj.getpiece(row, column)->type)
-                return true;
+                return en_valid;
         }
         // diagonal capture
         if(obj.getpiece(c2.x, c2.y) == nullptr)
-            return false;
+            return invalid;
         else if(obj.getpiece(c2.x, c2.y)-> team != obj.getpiece(row, column)-> team)
-            return true;
+            return gen_valid;
     }
     // invalidity
-    return false;
+    return invalid;
 }
 
-bool moveValidationKing(int row, int column, Coords c2, Board &obj)
+ReturnType moveValidationKing(int row, int column, Coords c2, Board &obj)
 {
     //***Threat Check not applied***
     int dx, dy;
     dx = c2.x - row;
     dy = c2.y - column;
     if(abs(dx) > 1 || abs(dy) > 1) // since 0 is applicable for parallel moves
-        return false;
+        return invalid;
     if(obj.getpiece(row + dx, column + dy)==nullptr)
-        return true;
+        return gen_valid;
     else if(obj.getpiece(row + dx, column + dy)->team != obj.getpiece(row,column)->team)
-        return true;
+        return gen_valid;
     else
-        return false;
+        return invalid;
 }
 
-bool moveValidation(Pieces piece, Coords c2, Board &obj)
+ReturnType moveValidation(Pieces piece, Coords c2, Board &obj)
 {
-    bool result;
     int row = piece.coords.x;
     int column = piece.coords.y;
     if (c2.x == row && c2.y == column) // current coords of the piece
-        return false;
+        return invalid;
 
     switch (piece.type)
     {
@@ -171,7 +169,10 @@ bool moveValidation(Pieces piece, Coords c2, Board &obj)
         return moveValidationBishop(row, column, c2, obj);
     case 'q': // white queen
     case 'Q': // black queen
-        return (moveValidationBishop(row, column, c2, obj)||moveValidationRook(row,column,c2, obj));
+        if(moveValidationBishop(row, column, c2, obj) != invalid||moveValidationRook(row,column,c2, obj)!=invalid)
+            return gen_valid;
+        else
+            return invalid;
     case 'n': // white knight
     case 'N': // black knight
         return moveValidationKnight(row,column, c2, obj);
@@ -180,6 +181,6 @@ bool moveValidation(Pieces piece, Coords c2, Board &obj)
         return moveValidationKing(row,column, c2, obj);
     default:
     printf("Error: moveValidations.invalidTeam");
-    return false;
+    return invalid;
     }
 }
--- moveValidations.h
+++ moveValidations.h
@@ -3,8 +3,15 @@
 #include "Board.h"
 #define t_MOVVALID
 #ifdef t_MOVVALID
-bool moveValidation(Pieces, Coords, Board&);
-bool moveValidationRook(int, int, Coords, Board&);
-bool moveValidationBishop(int, int, Coords, Board&);
-bool moveValidationKnight(int, int, Coords, Board&, Pieces);
+typedef enum ReturnType
+{
+    en_valid, // enpassant
+    cas_valid, // castling
+    gen_valid, // general valid
+    invalid // not valid
+}ReturnType;
+ReturnType moveValidation(Pieces, Coords, Board&);
+ReturnType moveValidationRook(int, int, Coords, Board&);
+ReturnType moveValidationBishop(int, int, Coords, Board&);
+ReturnType moveValidationKnight(int, int, Coords, Board&, Pieces);
 #endif
